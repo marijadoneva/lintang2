81a281542692160acd43218bdbd14abf8d866388
Marija <marijadoneva9@gmail.com>
2023-08-09 15:17:04 -0400

    Login Method

--- src/test/java/com/lintang2/pages/LoginPage.java
+++ src/test/java/com/lintang2/pages/LoginPage.java
@@ -1,4 +1,45 @@
 package com.lintang2.pages;
 
+import com.lintang2.utilities.ConfigurationReader;
+import com.lintang2.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement emailBox;
+
+    @FindBy(id = "inputPassword")
+    public WebElement passwordBox;
+
+    @FindBy(tagName = "button")
+    public WebElement loginButton;
+
+
+
+    public void login(String userType){
+
+        String username=ConfigurationReader.getProperty(userType+"_username");
+        String password= ConfigurationReader.getProperty("password");
+
+
+        emailBox.sendKeys(username);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+
+    }
+
+    public void login(String username,String password){
+
+        emailBox.sendKeys(username);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+
+    }
 }
